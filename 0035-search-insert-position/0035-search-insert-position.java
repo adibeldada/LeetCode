c4e388542c6eb70ff70c7101f55b74a3847525b9
Adib El Dada <adib.eldada@gmail.com>
2026-01-18 23:43:47 -0500

    Time: 0 ms (100%), Space: 44.8 MB (54.83%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int number = 0;
+        if (target < nums[0]){
+            return 0;
+        }
+
+        if (target > nums[nums.length-1]){
+            return (nums.length);
+        }
+
+        for (int i = 0; i < nums.length; i++){
+            if (target == nums[i]){
+                return i;
+            }
+            
+            if (target > nums[i] && target < nums[i+1]){
+                number = i+1;
+            }
+            
+        }
+        return number;
+    }
+}
