257fc0f9f2fba6bd87694ecde7e3ea7c887561de
Adib El Dada <adib.eldada@gmail.com>
2026-01-26 19:38:11 -0500

    Time: 0 ms (100%), Space: 48.5 MB (41.69%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,10 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++){
-            if (target == nums[i]){
-                return i;
+        int start=0;
+        int end =nums.length-1;
+        int result = binarysearch(nums,start,end,target);
+        return result;
+    }
+    private int binarysearch(int[] a,int l, int r,int x){
+        while (l <= r){
+            
+            int m = (l + r) / 2;
+
+
+            if (a[m] == x) {
+                return m;
+            }
+            else if (a[m] > x) {
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
             }
         }
+
         return -1;
     }
 }
