4bad751b9f37dc46764d1d0bf0fb7220c5a98828
Adib El Dada <adib.eldada@gmail.com>
2026-01-26 19:20:34 -0500

    Time: 1 ms (99.85%), Space: 47.2 MB (11.85%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,24 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int times = 0;
-    
-        for (int i = 0; i < nums.length; i++){
-            times = 0;
-            
-            for (int j = 0; j < nums.length; j++){
-                if (nums[i] == nums[j]){
-                    times++;
-                } 
-                
-            }
-
-            if (times != 2){
-                    return nums[i];
-                }
-
-
-
+        int result = 0;
+        for (int n : nums) {
+            result ^= n;
         }
-      return times;
+        return result;
     }
 }
